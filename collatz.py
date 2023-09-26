8d404d33949ce338c52541a5e533191aa55ce632
Bright Djogatse <brightdjogatse@gmail.com>
2023-09-26 07:49:11 +0000

    collatz file

---
+++ collatz.py
@@ -0,0 +1,24 @@
+# Bright Djogatse
+# collatz sequence
+#!/usr/bin/python3
+
+
+def collatz(number):
+    """collatz function"""
+    if number % 2 == 0:
+        print("number // 2")
+        return number
+    else:
+        number % 2 == 1
+        print("3 * number + 1")
+        return 3 * number + 1
+
+if __name__ == "__main__":
+
+    try:
+        user =int(input("Enter number:"))
+        result = collatz(user)
+        while (result != 1):
+            result = collatz(result)
+    except ValueError:
+        print("number must be an inter")
